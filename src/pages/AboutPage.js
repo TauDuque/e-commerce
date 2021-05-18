34dd1f92a4b404bd1b4de369a7a31b8843d9155c
TauDuque <hectauduque@gmail.com>
2021-05-17 23:30:06 -0300

    home and newsletter

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,11 +1,32 @@
-import React from 'react'
-import styled from 'styled-components'
-import { PageHero } from '../components'
-import aboutImg from '../assets/hero-bcg.jpeg'
+import React from "react";
+import styled from "styled-components";
+import { PageHero } from "../components";
+import aboutImg from "../assets/hero-bcg.jpeg";
 
 const AboutPage = () => {
-  return <h4>about page</h4>
-}
+  return (
+    <main>
+      <PageHero title="about" />
+      <Wrapper className="page section section-center">
+        <img src={aboutImg} alt="nice desk" />
+        <article>
+          <div className="title">
+            <h2>our history</h2>
+            <div className="underline"></div>
+          </div>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Esse
+            exercitationem, quae eos veritatis ratione reiciendis eaque nemo
+            ullam. Porro odit ea aliquam. Aspernatur, sunt. At natus nobis
+            quaerat id. Obcaecati unde iure explicabo dolorem molestiae
+            praesentium iusto ipsam dolorum quae, dolore nulla beatae eos
+            mollitia magnam nisi repellendus? Natus, nemo!
+          </p>
+        </article>
+      </Wrapper>
+    </main>
+  );
+};
 
 const Wrapper = styled.section`
   display: grid;
@@ -33,5 +54,5 @@ const Wrapper = styled.section`
   @media (min-width: 992px) {
     grid-template-columns: 1fr 1fr;
   }
-`
-export default AboutPage
+`;
+export default AboutPage;
