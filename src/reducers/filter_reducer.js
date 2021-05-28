2d977e7101aa955a2cdb323f8a7a269d8434faa4
TauDuque <hectauduque@gmail.com>
2021-05-28 00:05:25 -0300

    filter prices

--- src/reducers/filter_reducer.js
+++ src/reducers/filter_reducer.js
@@ -11,10 +11,13 @@ import {
 
 const filter_reducer = (state, action) => {
   if (action.type === LOAD_PRODUCTS) {
+    let maxPrice = action.payload.map((prod) => prod.price);
+    maxPrice = Math.max(...maxPrice);
     return {
       ...state,
       all_products: [...action.payload],
       filtered_products: [...action.payload],
+      filters: { ...state.filters, max_price: maxPrice, price: maxPrice },
     };
   }
   if (action.type === SET_GRIDVIEW) {
@@ -49,6 +52,13 @@ const filter_reducer = (state, action) => {
       ...state,
       filtered_products: tempProducts,
     };
+  }
+  if (action.type === UPDATE_FILTERS) {
+    const { name, value } = action.payload;
+    return { ...state, filters: { ...state.filters, [name]: value } };
+  }
+  if (action.type === FILTER_PRODUCTS) {
+    return { ...state };
   }
   throw new Error(`No Matching "${action.type}" - action type`);
 };
